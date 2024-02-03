b7c9c544f3d8c1b143f78ebd3063073c307940f9
wnx82 <vandermeulen.christophe@gmail.com>
2024-02-03 15:56:03 +0100

    Ajoutez des fichiers projet.

---
+++ Entities/Class.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Entities
+{
+    public class Employe
+    {
+
+        [Key]
+        [Column("Id_Employe")]
+        public Guid idEmpoloye { get; set; }
+
+        [Column("Nom")]
+        public string? Nom { get; set; }
+
+        [Column("Prenom")]
+        public string? Prenom { get; set; }
+
+
+        [Column("Birthday")]
+        public DateTime? Birthday { get; set; }
+
+
+    }
+}
---
+++ StateContainer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//coucou
+using CRUD.Entities;
+
+namespace CRUD
+{
+    public class StateContainer
+    {
+
+        #region Employe
+        private List<Employe> employes= new();
+        private Employe? selectedEmploye = null;
+
+        public List<Employe> Employes
+        {
+            get => employes;
+            set
+            {
+                employes = value;
+                NotifyEmployesUpdated();
+            }
+        }
+
+        public Employe? SelectedEmploye
+        {
+            get => selectedEmploye;
+            set
+            {
+                selectedEmploye = value;
+                OnEmployeSelected?.Invoke();
+            }
+        }
+
+        public event Action? OnEmployeUpdated;
+        public event Action? OnEmployeSelected;
+
+        public void NotifyEmployesUpdated() => OnEmployeUpdated?.Invoke();
+        #endregion
+    }
+}
